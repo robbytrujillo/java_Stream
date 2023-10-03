f8c9b86797002119e52ed173682f140a8c58599b
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-03 10:23:04 +0700

    Java Stream | Create Stream Test

--- src/test/java/programmer/zaman/now/stream/CreateStreamTest.java
+++ src/test/java/programmer/zaman/now/stream/CreateStreamTest.java
@@ -2,6 +2,9 @@ package programmer.zaman.now.stream;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class CreateStreamTest {
@@ -15,6 +18,32 @@ public class CreateStreamTest {
 
         String data = null;
         Stream<String> emptyOrNotStream = Stream.ofNullable(data);
+
+    }
+
+    // kode menjalankan stream
+    @Test
+    void testCreateStreamFromArray() {
+
+        Stream<String> arrayStream = Stream.of("Robby", "Ilham", "Kusuma");
+
+        arrayStream.forEach(value -> {
+            System.out.println(value);
+        });
+
+        Stream<Integer> streamInteger = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        streamInteger.forEach(value -> {
+            System.out.println(value);
+        });
+
+        // Stream<String> streamArray = Arrays.stream(new String[] {"Robby", "Ilham", "Kusuma"});
+
+        String[] array = new String[] {
+                "Robby", "Ilham", "Kusuma"
+        };
+
+        Stream<String> streamFromArray = Arrays.stream(array);
 
     }
 }
