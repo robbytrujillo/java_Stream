6d6b9b953e04a6b609f5dc1555039e79fbfe0aac
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-05 09:52:41 +0700

    Java Stream | Stream-Operations

--- src/test/java/programmer/zaman/now/stream/CreateStreamBuilderTest.java
+++ src/test/java/programmer/zaman/now/stream/CreateStreamBuilderTest.java
@@ -25,4 +25,5 @@ public class CreateStreamBuilderTest {
 
         stream.forEach(System.out::println);
     }
+
 }
---
+++ src/test/java/programmer/zaman/now/stream/StreanOperationTest.java
@@ -0,0 +1,20 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import java.util.stream.Stream;
+
+public class StreanOperationTest {
+
+    @Test
+    void testStreamOperation() {
+
+        List<String> names = List.of("Robby", "Ilham", "Kusuma");
+
+        Stream<String> streamNames = names.stream();
+        Stream<String> streamUpper = streamNames.map(name -> name.toUpperCase()); // map itu dugunakan untuk mengkonversi dari suatu data asli menjadi data baru
+
+    }
+}
