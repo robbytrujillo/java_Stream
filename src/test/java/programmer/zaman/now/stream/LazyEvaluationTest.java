e5f868363119ef0f6503460159b44e8bc5723857
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-07 22:32:30 +0700

    Java Stream | Lazy-Evaluation

---
+++ src/test/java/programmer/zaman/now/stream/LazyEvaluationTest.java
@@ -0,0 +1,15 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class LazyEvaluationTest {
+
+    @Test
+    void testIntermediateOperation() {
+
+        List<String> names = List.of("Robby", "Ilham", "Kusuma");
+
+    }
+}
