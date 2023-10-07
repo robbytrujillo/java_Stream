6a3cbd40d27f1b94ddc2e9e616fe9e95fe92b9c4
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-07 22:59:11 +0700

    Java Stream | Lazy-Evaluation

--- src/test/java/programmer/zaman/now/stream/LazyEvaluationTest.java
+++ src/test/java/programmer/zaman/now/stream/LazyEvaluationTest.java
@@ -3,6 +3,7 @@ package programmer.zaman.now.stream;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class LazyEvaluationTest {
 
@@ -10,6 +11,12 @@ public class LazyEvaluationTest {
     void testIntermediateOperation() {
 
         List<String> names = List.of("Robby", "Ilham", "Kusuma");
+
+        Stream<Object> upper = names.stream()
+                .map(name -> {
+                    System.out.println("Change" + name + "to Uppercase");
+                return name.toUpperCase();
+                });
 
     }
 }
