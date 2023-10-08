31c3f0b7d6ca38daeca6ff039e4be9724cb3ae89
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-09 05:48:50 +0700

    Java Stream | Transformation Opertiaons

---
+++ src/test/java/programmer/zaman/now/stream/TransformationOperationTest.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class TransformationOperationTest {
+
+    @Test
+    void testMap() {
+        List.of("Robby", "Ilham", "Kusuma");
+    }
+}
