65ef3308c95dd324651613465660895b5e35f660
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-10 05:59:37 +0700

    Java Stream | Filtering Opertiaons

---
+++ src/test/java/programmer/zaman/now/stream/FilteringOperationTest.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class FilteringOperationTest {
+
+    @Test
+    void testFilter() {
+        List<String> names = List.of("Robby", "Ilham", "Kusuma")
+    }
+}
