59ab6fbf5bcd33a6a35cd6d7780d771f93f1e6d6
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-10 09:41:58 +0700

    Retrieving Operation

---
+++ src/test/java/programmer/zaman/now/stream/RetrievingOperationTest.java
@@ -0,0 +1,22 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class RetrievingOperationTest {
+
+    @Test
+    void testLimit() {
+        List.of("Robby", "Ilham", "Kusuma", "Asep", "Tatang").stream()
+                .limit(2)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    void testSkip() {
+        List.of("Robby", "Ilham", "Kusuma", "Asep", "Tatang").stream()
+                .skip(2)
+                .forEach(System.out::println);
+    }
+}
