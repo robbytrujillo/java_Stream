2193f01fc7763ed91016d8c35226731b33e1d5d9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-12 06:24:36 +0700

    Aggregate Operation

--- src/test/java/programmer/zaman/now/stream/AggregateOperationTest.java
+++ src/test/java/programmer/zaman/now/stream/AggregateOperationTest.java
@@ -23,8 +23,15 @@ public class AggregateOperationTest {
 
     @Test
     void testCount() {
-        Long count = List.of("Robby", "Ilham", "Kusuma", "Programmer", "Zaman", "Now").stream()
+        long count = List.of("Robby", "Ilham", "Kusuma", "Programmer", "Zaman", "Now").stream()
                 .count();
         System.out.println(count);
+    }
+
+    // Reduce Operator
+    @Test
+    void testSum() {
+        List.of(1, 2, 3, 4, 5).stream()
+                .reduce(0, (value, item) -> value + item);
     }
 }
