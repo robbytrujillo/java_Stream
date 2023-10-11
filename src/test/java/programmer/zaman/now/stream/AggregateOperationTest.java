40e2407eb44f34af031fd6a6c723a34474bdbbd2
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-12 06:46:27 +0700

    Aggregate Operation

--- src/test/java/programmer/zaman/now/stream/AggregateOperationTest.java
+++ src/test/java/programmer/zaman/now/stream/AggregateOperationTest.java
@@ -36,5 +36,27 @@ public class AggregateOperationTest {
                 .reduce(0, (value, item) -> value + item);
 
         System.out.println(result);
+
+        // 0
+        // 1. ==> value = 0 item = 1 = 1
+        // 2. ==> value = 1 item = 2 = 3
+        // 3. ==> value = 3 item = 3 = 6
+        // 4. ==> value = 6 item = 4 = 10
+        // 5. ==> value = 10 item = 5 = 15
+    }
+
+    @Test
+    void testFactorial() {
+        var factorial = List.of(1, 2, 3, 4, 5).stream()
+                .reduce(1, (value, item) -> value * item);
+
+        System.out.println(factorial);
+
+        // 0
+        // 1. ==> value = 1 item = 1 = 1
+        // 2. ==> value = 1 item = 2 = 2
+        // 3. ==> value = 2 item = 3 = 6
+        // 4. ==> value = 6 item = 4 = 24
+        // 5. ==> value = 24 item = 5 = 120
     }
 }
