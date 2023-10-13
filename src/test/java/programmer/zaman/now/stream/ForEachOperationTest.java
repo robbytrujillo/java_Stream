ea8f818eaeabf4d73c3dbba2117686953fb4a28e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-14 06:29:34 +0700

    Java Stream | For Each Operation

--- src/test/java/programmer/zaman/now/stream/ForEachOperationTest.java
+++ src/test/java/programmer/zaman/now/stream/ForEachOperationTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class ForEachOperationTest {
 
     @Test
-    void testPeek() {
+    void testPeekBefore() {
         List.of("Robby", "Ilham", "Kusuma", "Programmer", "Zaman", "Now").stream()
                 .map(name ->{
                     System.out.println("Before Change Name to Upper : " + name);
@@ -15,6 +15,15 @@ public class ForEachOperationTest {
                     System.out.println("Change name to Upper : " + upper);
                     return upper;
                 } )
+                .forEach(name -> System.out.println("Final name : " + name));
+    }
+
+    @Test
+    void testPeekAfter() {
+        List.of("Robby", "Ilham", "Kusuma", "Programmer", "Zaman", "Now").stream()
+                .peek(name -> System.out.println("Before Change Name to Upper : " + name))
+                .map(name -> name.toUpperCase())
+                .peek(upper -> System.out.println("Change name to Upper : " + upper))
                 .forEach(name -> System.out.println("Final name : " + name));
     }
 }
