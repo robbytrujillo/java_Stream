04aed087d5453eb2f5a771b3723405b03fce1ed0
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-16 04:01:38 +0700

    Java Stream | Primitive Stream

--- src/test/java/programmer/zaman/now/stream/PrimitiveStreamTest.java
+++ src/test/java/programmer/zaman/now/stream/PrimitiveStreamTest.java
@@ -2,7 +2,9 @@ package programmer.zaman.now.stream;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class PrimitiveStreamTest {
 
@@ -10,5 +12,9 @@ public class PrimitiveStreamTest {
     void testCreate() {
         IntStream intStream = IntStream.range(1, 100);
         intStream.forEach(System.out::println);
+
+        LongStream longStream = LongStream.of(1, 2, 3, 4);
+
+        DoubleStream doubleStream = DoubleStream.builder().add(0.1).add(0.2).build();
     }
 }
