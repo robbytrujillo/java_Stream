d8d85b01ce1a4d3b6abe830e277374e2ccafb47b
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-17 12:52:17 +0700

    Java Stream | Collectors

---
+++ src/test/java/programmer/zaman/now/stream/CollectorsTest.java
@@ -0,0 +1,24 @@
+package programmer.zaman.now.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectorsTest {
+
+    Stream<String> getStream() {
+        return Stream.of("Robby", "Ilham", "Kusuma");
+    }
+
+    @Test
+    void testCollection() {
+        Set<String> set = getStream().collect(Collectors.toSet());
+        Set<String> immutableSet = getStream().collect(Collectors.toUnmodifiableSet());
+
+        List<String> list = getStream().collect(Collectors.toList());
+        List<String> immutableList = getStream().collect(Collectors.toUnmodifiableList());
+    }
+}
